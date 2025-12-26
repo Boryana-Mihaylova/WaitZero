475e01602b35f67c56f0236731f0ca019345ecb6
Boryana Mihaylova <130145291+BoryanaMihaylova@users.noreply.github.com>
2025-12-26 15:42:26 +0200

    remove old Category entities and related service and repository

--- src/main/java/dev/waitzero/waitzero/model/entity/CategoryName.java
+++
@@ -1,6 +0,0 @@
-package dev.waitzero.waitzero.model.entity;
-
-public enum CategoryName {
-
-    PEDESTRIAN, BICYCLE, MOTORCYCLE, CAR;
-}
--- src/main/java/dev/waitzero/waitzero/service/CategoryService.java
+++
@@ -1,9 +0,0 @@
-package dev.waitzero.waitzero.service;
-
-import dev.waitzero.waitzero.model.entity.Category;
-import dev.waitzero.waitzero.model.entity.CategoryName;
-
-public interface CategoryService {
-
-    Category findCategoryByName(CategoryName categoryName);
-}
