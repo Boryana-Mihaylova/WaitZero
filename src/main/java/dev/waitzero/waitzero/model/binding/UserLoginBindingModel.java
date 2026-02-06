a052c2f8ae54ab587d2fb18afda84e060559ba82
Boryana Mihaylova <130145291+Boryana-Mihaylova@users.noreply.github.com>
2026-02-06 16:24:54 +0200

    refine validation rules for user forms

--- src/main/java/dev/waitzero/waitzero/model/binding/UserLoginBindingModel.java
+++ src/main/java/dev/waitzero/waitzero/model/binding/UserLoginBindingModel.java
@@ -1,7 +1,16 @@
 package dev.waitzero.waitzero.model.binding;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public class UserLoginBindingModel {
+
+    @NotBlank(message = "Username is required")
+    @Size(min = 3, max = 20, message = "Username must be between 3 and 20 characters")
     private String username;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 5, max = 20, message = "Password must be between 5 and 20 characters")
     private String password;
 
     public UserLoginBindingModel() {
--- src/main/java/dev/waitzero/waitzero/model/binding/UserRegisterBindingModel.java
+++ src/main/java/dev/waitzero/waitzero/model/binding/UserRegisterBindingModel.java
@@ -1,8 +1,20 @@
 package dev.waitzero.waitzero.model.binding;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public class UserRegisterBindingModel {
+
+    @NotBlank(message = "Username is required")
+    @Size(min = 3, max = 20, message = "Username must be between 3 and 20 characters")
     private String username;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 5, max = 20, message = "Password must be between 5 and 20 characters")
     private String password;
+
+    @NotBlank(message = "Confirm password is required")
+    @Size(min = 5, max = 20, message = "Confirm password must be between 5 and 20 characters")
     private String confirmPassword;
 
     public UserRegisterBindingModel() {
